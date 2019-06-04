e06a2b90717a7b1569f752df23b0e857320b126d
Student <magnusarbuthnott@gmail.com>
2019-06-04 18:24:21 +0100

    Test GREEN. Updated gemfile with 'sinatra-flash'. Removed auto 'save_and_open' when in rackup from login_spec.rb. Created 'post /sessions' in app.rb.

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
-require 'rubygems'
+# require 'rubygems'
 require 'sinatra/base'
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'sinatra/flash'
 
 require './lib/setup_database'
 require './lib/space'
 require './lib/user'
 
 class Makers_BnB < Sinatra::Base
+ 
+  configure :development do
+    register Sinatra::Flash
+  end
+
   enable :sessions
 
   setup_database
@@ -16,7 +22,22 @@ class Makers_BnB < Sinatra::Base
     erb :index
   end
 
-  post "/" do
+  post '/sessions' do
+    user = User.all(:username => params[:username]) & User.all(:password => params[:password])
+    if user
+      session[:user_id] = user[0].id
+      redirect '/spaces'
+    else
+      flash[:notice] = 'The username and password that you entered did not match our records. Please double-check and try again.'
+      redirect '/'
+    end
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
     @user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
     redirect "/spaces"
   end
--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -4,8 +4,8 @@ feature 'logging in' do
     user = User.create(name: 'Roxana Smith', username: 'SaltyRox', email: 'roxxmoxx@gmail.com', password: 'roxurulez1234')
 
     visit('/')
-    fill_in('login-username', with: 'SaltyRox')
-    fill_in('login-password', with: 'roxurulez1234')
+    fill_in('username', with: 'SaltyRox')
+    fill_in('password', with: 'roxurulez1234')
     click_button('Login')
     expect(current_path).to eq('/spaces')
   end
