95f2b1edd6a26d1f66380633a071e74df70e743b
Sam <samkitchen94@gmail.com>
2019-06-07 11:15:23 +0100

    tidied up slightly

--- lib/setup_database.rb
+++ lib/setup_database.rb
@@ -2,12 +2,10 @@ require './lib/user'
 require './lib/space'
 
   if ENV['ENVIRONMENT'] == 'test'
-    p 'should be in test db'
     DataMapper.setup(
       :default, 'postgres://:@localhost/makers_bnb_test'
     )
   else
-    p 'should be in dev database'
     DataMapper.setup(
       :default, 'postgres://:@localhost/makers_bnb'
   )
